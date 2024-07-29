b9f9021176807f43d05af0f7aa73e47da6287ab0
Marcin <marcinchodkowski004@wp.pl>
2024-07-29 14:08:21 +0200

    text centered

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -4,7 +4,7 @@ import Form from '@/components/Form';
 export default function Page() {
 	return (
 		<main className='px-4'>
-			<h1>Create a post</h1>
+			<h1 className='text-center text-3xl pt-5 md:text-4xl md:pt-8'>Create a post</h1>
 
 			<Form />
 		</main>
