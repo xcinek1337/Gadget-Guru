3155c2e3843b4066a02a122d55721dbf9dd6722a
Marcin <marcinchodkowski004@wp.pl>
2024-07-29 14:07:32 +0200

    form styling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,26 +5,25 @@ export default function Form() {
 	return (
 		<form
 			action={createPost}
-			className=' max-w-[400px] flex flex-col px-2 py-4 gap-2 mx-auto my-10 border-8 border-gray-300 rounded-lg bg-white'
+			className=' max-w-[500px] flex flex-col px-4 py-8 gap-2 mx-auto mt-8 rounded-xl bg-white md:text-lg'
 		>
-			<h1 className='border-2 w-fit mx-auto px-3.5 py-2 text-3xl bg-yellow-300 text-slate-500 font-semibold rounded-full'>
-				Create Post
-			</h1>
 			<input
-				className='border bg-gray-100 border-gray-500 rounded px-3 h-10'
+				id='title'
+				className='border-2 bg-gray-100 border-gray-200 rounded px-3 h-10 focus:ring-0 focus:outline-none focus:border-2 focus:border-yellow-400 transition md:py-7 '
 				type='text'
 				name='title'
-				placeholder='title for new post...'
+				placeholder='Title...'
 				required
 			/>
+
 			<textarea
 				name='body'
-				placeholder='Body  content for new post'
-				className='border bg-gray-100 border-gray-500 rounded px-3 py-2'
-				rows={9}
+				placeholder='Body content for new post...'
+				className='border-2 bg-gray-100 border-gray-200 rounded px-3 py-2 focus:ring-0 focus:outline-none focus:border-2 focus:border-yellow-400 transition'
+				rows={12}
 				required
 			/>
-			<button className='h-10 bg-blue-500 px-5 rounded text-white'>suybbmit</button>
+			<button className='py-2 bg-yellow-500 px-5 rounded text-gray-50 font-bold  tracking-widest hover:bg-yellow-600 transition '>Submit</button>
 		</form>
 	);
 }
