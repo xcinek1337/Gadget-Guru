2783b746903578951bcb54871d830d47c3df42a9
Marcin <marcinchodkowski004@wp.pl>
2024-07-31 16:32:50 +0200

    adding zod validation to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,9 +3,14 @@
 import createPost from '@/actions/createPost';
 import { FormEvent, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
+import z, { ZodError } from 'zod';
+
+const Post = z.object({
+	title: z.string().trim().min(1, 'Title is required'),
+	body: z.string().trim().min(1, 'Body content is required'),
+});
 
 export default function Form() {
-	const notify = () => toast.success('Post published ');
 	const [isSubmitting, setIsSubmitting] = useState(false);
 
 	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
@@ -14,14 +19,24 @@ export default function Form() {
 
 		const form = e.currentTarget;
 		const formData = new FormData(form);
+		const body = formData.get('body') as string;
+		const title = formData.get('title') as string;
 
 		try {
+			Post.parse({
+				title,
+				body,
+			});
+
 			await createPost(formData);
-			notify();
+			toast.success('Post published ');
 			form.reset();
 		} catch (error) {
-			toast.error('Failed to publish post');
-			console.error('Error creating post:', error);
+			if (error instanceof ZodError) {
+				toast.error(error.errors[0]?.message);
+			} else {
+				console.log(error);
+			}
 		} finally {
 			setIsSubmitting(false);
 		}
@@ -36,18 +51,19 @@ export default function Form() {
 				id='title'
 				className='border-2 bg-gray-100 border-gray-200 rounded px-3 h-10 focus:ring-0 focus:outline-none focus:border-2 focus:border-yellow-400 transition md:py-7 '
 				type='text'
-				name='title'
 				placeholder='Title...'
-				required
+				name='title'
 			/>
+			{/* {errors.title && <p className='text-sm text-red-500'>{errors.title.message}</p>} */}
 
 			<textarea
-				name='body'
 				placeholder='Body content for new post...'
 				className='border-2 bg-gray-100 border-gray-200 rounded px-3 py-2 focus:ring-0 focus:outline-none focus:border-2 focus:border-yellow-400 transition'
 				rows={12}
-				required
+				name='body'
 			/>
+			{/* {errors.body && <p className='text-sm text-red-500'>{errors.body.message}</p>} */}
+
 			<button
 				className={`py-2 bg-yellow-500 px-5 rounded text-gray-50 font-bold tracking-widest transition ${
 					isSubmitting ? 'cursor-not-allowed' : 'hover:bg-yellow-600'
