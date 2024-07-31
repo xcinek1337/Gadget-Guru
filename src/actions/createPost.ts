f5d4e452832d3b167731bd690fbde40bf5e2f16f
Marcin <marcinchodkowski004@wp.pl>
2024-07-31 16:32:38 +0200

    changed name file

--- src/actions/createPost.ts
+++ src/actions/createPost.ts
@@ -1,25 +1,24 @@
-'use server'
-
-import prisma from '@/db'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
+'use server';
+import prisma from '@/db';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export default async function createPost(FormData: FormData) {
-	const { isAuthenticated } = getKindeServerSession()
-	const body = FormData.get('body') as string
-	const title = FormData.get('title') as string
+	const { isAuthenticated } = getKindeServerSession();
+	const body = FormData.get('body') as string;
+	const title = FormData.get('title') as string;
 
-    // check if user is already logged
+	// check if user is already logged
 	if (!(await isAuthenticated())) {
-		redirect('/api/auth/login')
+		redirect('/api/auth/login');
 	}
 	await prisma.post.create({
 		data: {
 			title,
 			body,
 		},
-	})
+	});
 
-    revalidatePath('/posts')
+	revalidatePath('/posts');
 }
